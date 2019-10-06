d955a668615b74f15d62ad8f4c36c7a089ffd8f5
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-06 12:56:24 -0600

    Add style to side menu

--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -12,7 +12,16 @@ class SideMenu extends Component {
   render() {
       return(
         <section>
-            <i className="fas fa-bars"></i>
+            <div className='menu-button'>
+              <i className="fas fa-bars"></i>
+            </div>
+            <nav>
+              <ul className='menu-items'>
+                <li className='menu-list'>Your Information</li>
+                <li className='menu-list'>Sign out</li>
+              </ul>
+            </nav>
+
         </section>
       )
   }
