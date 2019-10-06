eb7c5a13f0ddd22d4196fdd89ced77464a39fb03
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-06 10:25:26 -0600

    Add jsx and methods to searchProperty component

---
+++ src/SearchProperty/SearchProperty.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class SearchProperty extends Component {
+    constructor() {
+      super();
+      this.state = {
+         location: ''
+      }
+    }
+
+    handleChange = (e) => {
+      this.state({ [e.target.name] : e.target.value })
+    }
+
+    submitSearch = (e) => {
+      e.preventDefault();
+
+      // method in app goes here to search property by location
+
+      this.setState({ location: '' })
+    }
+
+    render() {
+        return(
+          <form>
+              <input className='search-property' 
+                     type='text'
+                     name='location' 
+                     value={this.state.location}
+                     onChange={(e) => this.handleChange(e)}/>
+              <button className='search-button' onClick={(e) => {this.submitSearch(e)}}>Search</button>
+          </form>
+        )
+    }
+}
+
+export default SearchProperty;
