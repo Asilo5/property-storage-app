d27cbd94de971a4bc1d445dbb04e95ae9ded50ef
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-06 12:24:48 -0600

    Add sideMenu component for hamburger menu

---
+++ src/SideMenu/SideMenu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './SideMenu.scss';
+
+class SideMenu extends Component {
+  constructor() {
+      super();
+      this.state = {
+
+      }
+  }
+
+  render() {
+      return(
+        <p>Side Bar location</p>
+      )
+  }
+}
+
+export default SideMenu;
