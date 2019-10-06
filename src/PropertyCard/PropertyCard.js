45e21077f93310c0fa878e87143cf2f0523c0577
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-06 10:33:46 -0600

    Add jsx to property card component

---
+++ src/PropertyCard/PropertyCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PropertyCard.scss';
+
+const PropertyCard = () => {
+    return(
+      <section className='property-card'>
+
+      </section>
+    )
+}
+
+export default PropertyCard;
