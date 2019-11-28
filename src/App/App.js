2ec560ad0756b3e96fb3787787ebbd3594238b27
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-28 11:24:37 -0700

    Delete sidemenu component and add a folder for App

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './reset.scss';
+import '../reset.scss';
 import './App.scss';
 
-import Header from './Header/Header';
-import SearchProperty from './SearchProperty/SearchProperty';
-import PropertyFiltering from './PropertyFiltering/PropertyFiltering';
-import PropertyContainer from './PropertyContainer/PropertyContainer';
+import Header from '../Header/Header';
+import SearchProperty from '../SearchProperty/SearchProperty';
+import PropertyFiltering from '../PropertyFiltering/PropertyFiltering';
+import PropertyContainer from '../PropertyContainer/PropertyContainer';
 
 class App extends Component {
   constructor() {
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react'; 
 import './Header.scss';
-import SideMenu from '../SideMenu/SideMenu';
 
 const Header = () => {
   return(
     <header>
         <h1>Proplr</h1>
-        <SideMenu />
+        
     </header> 
   )
 }
