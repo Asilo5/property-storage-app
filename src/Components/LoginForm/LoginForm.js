2d113ac4198daa9c572aa97c3710e7034f32e9ee
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-28 17:47:04 -0700

    Add form to LoginForm

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -5,16 +5,26 @@ class LoginForm extends Component {
     constructor() {
       super();
       this.state = {
-          userName: '',
           userEmail: '',
-          userPassword: ''
+          userPassword: '',
+          isLoggedIn: false
       }
     }
 
     render() {
         return (
             <form>
-                
+                 <input placeholder='youremail@here.com' 
+                        type='email' 
+                        name='userEmail' 
+                        value={this.state.userEmail} 
+                        />
+                 <input placeholder='Password' 
+                        type='password' 
+                        name='userPassword' 
+                        value={this.state.userPassword} 
+                        />
+                 <button> Submit </button>
             </form>
         )
     }
