c5c1f480384db896455d0555361265b77b8f52f7
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-28 17:31:47 -0700

    Add basic setup of LoginForm component

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Login.scss';
+
+class LoginForm extends Component {
+    constructor() {
+      super();
+      this.state = {
+          userName: '',
+          userEmail: '',
+          userPassword: ''
+      }
+    }
+
+    render() {
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
+
+export default LoginForm;
